3878cfc72517979793785030e5d4b87014ad8265
meganods <meganods@users.noreply.github.com>
2026-07-17 17:10:50 +0530

    Import missing shared_preferences and firebase_auth packages in settings_screen.dart

--- urbanuser/lib/screens/settings_screen.dart
+++ urbanuser/lib/screens/settings_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class SettingsScreen extends StatefulWidget {
   static const routeName = '/settings';

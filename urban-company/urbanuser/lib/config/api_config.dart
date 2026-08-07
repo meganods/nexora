70aa8d4ed518888c1cfa229a308f1629b66dc81c
vishalratanshakya <vishalratanshakya@gmail.com>
2026-08-07 14:53:50 +0530

    chore: restore Render URL

--- urban-company/urbanuser/lib/config/api_config.dart
+++ urban-company/urbanuser/lib/config/api_config.dart
@@ -4,7 +4,7 @@ import 'dart:io' show Platform;
 class ApiConfig {
   /// Change this to your live Render URL once deployed!
   /// Example: 'https://nexora-backend.onrender.com'
-  static const String liveBackendUrl = ''; 
+  static const String liveBackendUrl = 'https://nexora-94dt.onrender.com'; 
 
   static String get baseUrl {
     if (liveBackendUrl.isNotEmpty) {
